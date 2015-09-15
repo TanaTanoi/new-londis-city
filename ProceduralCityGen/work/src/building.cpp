0d2f3af1a8df872f8414419ab271425de0727800
TanaTanoi <tana.tanoi@gmail.com>
2015-09-15 21:16:22 +1200

    Added building files
    Haven't put anything in them yet, still need to decide the format of the
    project

---
+++ ProceduralCityGen/work/src/building.cpp
@@ -0,0 +1,24 @@
+//---------------------------------------------------------------------------
+//
+// Copyright (c) 2015 Tana Tanoi
+//
+// In no event will the authors be held liable for any damages arising from
+// the use of this software.
+//
+// The contents of this file may not be copied or duplicated in any form
+// without the prior permission of its owner.
+//
+//----------------------------------------------------------------------------
+
+#include <cmath>
+#include <cstdlib>
+#include <iostream>
+#include <string>
+
+#include "comp308.hpp"
+#include "building.hpp"
+
+using namespace std;
+using namespace comp308;
+
+
---
+++ ProceduralCityGen/work/src/building.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+
+/*
+* building.hpp
+*
+*  Created on: 15/09/2015
+*      Author: tanoitana
+*/
+
+#include <cmath>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+#include "comp308.hpp"
