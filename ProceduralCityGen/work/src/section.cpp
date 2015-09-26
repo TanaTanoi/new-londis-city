2e138ee45c2df9660bb510c56f666b35448435ac
hc09141 <hannahcraighead23@gmail.com>
2015-09-26 17:10:53 +1200

    Changes are being made to the intersect metehods in the utility class

--- ProceduralCityGen/work/src/section.cpp
+++ ProceduralCityGen/work/src/section.cpp
@@ -8,15 +8,17 @@
 
 #include "section.hpp"
 #include "comp308.hpp"
+#include "utility.hpp"
 
 using namespace std;
 using namespace comp308;
+using namespace util;
 
 SectionDivider::SectionDivider() {};
 
 /**
-* Finds the longest edge in a section and returns it
-*/
+ * Finds the longest edge in a section and returns it
+ */
 line SectionDivider::findLongestEdge(section s) {
 	float maxLength = 0.0f;
 	line longLine;
@@ -41,61 +43,67 @@ line SectionDivider::findLongestEdge(section s) {
 //
 
 /**
-* Recursively divides sections by extending a perpendicular bisector
-* of the longest edge slightly off centre. Continuely recurses until
-* it is smaller than the largest acceptable size for a section.
-*/
+ * Recursively divides sections by extending a perpendicular bisector
+ * of the longest edge slightly off centre. Continuely recurses until
+ * it is smaller than the largest acceptable size for a section.
+ */
 
-void SectionDivider::recDivideSection(section *s) {
+void SectionDivider::recDivideSection(section s) {
 	vector<section> lots = splitSection(s);
 	for (section l : lots) {
-
-
-
-//		if (l.area <= goalArea) {
-//			sections.push_back(l);
-//		}
-//		else {
-//			recDivideSection(&l);
-//		}
+		splitSection(s);
 	}
+
+	//		if (l.area <= goalArea) {
+	//			sections.push_back(l);
+	//		}
+	//		else {
+	//			recDivideSection(&l);
+	//		}
 }
-//
-///**
-// Finds whether two lines intersect
-//*/
-//
 
 //
-//vector<section> SectionDivider::splitSection(section *s) {
-//	line* l = findLongestEdge(s);
-//	vec2 lineVec =  l->end - l->start;
-//	vec2 perpBi = vec2(-lineVec.y, lineVec.x); // gets perpendicular bisector to longest edge
-//
-//	// Randomly decides a position around the centre of the longest edge
-//	// to extend the bisector from. This will be within the middle third of the section
-//
-//	float random = ((float)rand() / (RAND_MAX)) + 1;
-//	float centre = l->length*(1.0f / 3.0f) + random * (l->length*(1.0f / 3.0f));
-//
-//
-//
-//	// Now finds the first intersection point with another line within the section
-//	vector<line *> intersectors;
-//	for (int i = 0; i < (int)s->lines.size(); i++) {
-//		if (intersects(l, m_l, c_l, &(s->lines[i]))) {
-//			intersectors.push_back(&(s->lines[i]));
-//		}
-//	}
-//	free(l); // frees pointer
-//
-//	// Now cretes two new sections from the split
-//
-//
-//	vector<section> x;
-//	return x;
-//
-//}
+vector<section> SectionDivider::splitSection(section s) {
+	line l = findLongestEdge(s);
+	vec2 lineVec =  l.end - l.start;
+	vec2 perpBi = vec2(-lineVec.y, lineVec.x); // gets perpendicular bisector to longest edge
+
+	// Randomly decides a position around the centre of the longest edge
+	// to extend the bisector from. This will be within the middle sixth of the section
+
+	float random = ((float)rand() / (RAND_MAX)) + 1;
+	float centreX = l.length*(5.0f / 12.0f) + random * (l.length*(1.0f / 6.0f));
+
+	float m = (l.end.y - l.start.y) / (l.start.x, l.end.x - l.start.x);
+	float c = l.end.y - m*l.end.x;
+
+	float centreY = m*centreX + c;
+
+
+
+	// Now finds the first intersection point with another line within the section
+	vector<line> intersectors;
+	for (int i = 0; i < (int)s.lines.size(); i++) {
+		if(s.lines[0].ID != l.ID){
+			if (intersects(s.lines[i],perpBi)) {
+				intersectors.push_back(s.lines[i]);
+			}
+		}
+	}
+
+	cout<< "Intersecting Lines found" << intersectors.size() << endl;
+
+	// Now creates two new sections from the split
+
+	line toCut = intersects[0];
+
+	section a, b; // the two new sections
+	line bi = {getIntersection(toCut,perpBi), getIntersection(l,perpBi)};
+
+	vector<section> x;
+	return x;
+
+}
 
 void SectionDivider::testSection() {
 	line a = {vec2(100,100),vec2(400,100),0};
@@ -112,26 +120,26 @@ void SectionDivider::testSection() {
 	section s = { lines };
 	sections.push_back(s);
 
-		for (line l : sections[0].lines) {
+	for (line l : sections[0].lines) {
 		cout << l.ID << endl;
-		}
+	}
 }
 
 void SectionDivider::renderTest() {
 	//section s = sections.back();
 	glBegin(GL_LINES);
 
-//	line longLine = findLongestEdge(sections[0]);
+	//	line longLine = findLongestEdge(sections[0]);
 	int longID  = findLongestEdge(sections[0]).ID;
-//	cout << "LongLIne: " << longLine.ID << endl;
+	//	cout << "LongLIne: " << longLine.ID << endl;
 	for (line l : sections[0].lines) {
-//		cout <<"Checking ID "<< l.ID << endl;
+		//		cout <<"Checking ID "<< l.ID << endl;
 		if(longID == l.ID){ // draw green if longest line
 			glColor3f(0.0,1.0,0.0);
-//			cout <<"Drawing longest line: "<< l.ID << endl;
+			//			cout <<"Drawing longest line: "<< l.ID << endl;
 		}
 		else{
-//			cout <<"Not     longest line: "<< l.ID << endl;
+			//			cout <<"Not     longest line: "<< l.ID << endl;
 			glColor3f(1.0,0.0,0.0); // otherwise draw red
 		}
 
@@ -139,7 +147,7 @@ void SectionDivider::renderTest() {
 		glVertex2f(l.end.x, l.end.y);
 	}
 	glEnd();
-	}
+}
 
 //void SectionDivider::cleanUp(){
 //	for(line * l : sections[0].lines){
--- ProceduralCityGen/work/src/utility.hpp
+++ ProceduralCityGen/work/src/utility.hpp
@@ -14,30 +14,34 @@ using namespace std;
 
 namespace util{
 
-bool intersects(line lon, line other) {
+bool intersects(line lon, vec2 cut) {
 
-	float m_l = (lon.end.y - lon.start.y) / (lon.start.x, lon.end.x - lon.start.x);
-	float c_l = lon.end.y - m_l*lon.end.x;
+	vec2 intersection = getIntersection(lon,cut);
 
+	// Now uses the intersection point of these two lines to determine if this is
+	//the line it should split
 
-	float m = (other.end.y - other.start.y) / (other.end.x - other.start.x);
-	float c = other.end.y - m*other.end.x;
-
-	if (lon.ID != lon.ID){
-
-		// Calculates the intersection point
-		float x = (c - c_l) / (m_l - m);
-		float y = m_l * x + c_l;
-
-		// Now uses the intersection point of these two lines to determine if this is
-		//the line it should split
-
-		if (x >= max(other.start.x, other.end.x) && x <= min(other.start.x, other.end.x)
-				&& y >= max(other.start.y, other.start.y) && y <= min(other.start.y, other.start.y)) {
-			return true;
-		}
+	if (intersection.x >= max(lon.start.x, lon.end.x) && intersection.x <= min(lon.start.x, lon.end.x)
+			&& y >= max(lon.start.y, lon.start.y) && y <= min(lon.start.y, lon.start.y)) {
+		return true;
 	}
+
 	return false;
+}
+
+vec2 getIntersection(line l, vec2 cut){
+	float m_l = (l.end.y - l.start.y) / (l.start.x, l.end.x - l.start.x);
+	float c_l = l.end.y - m_l*l.end.x;
+
+
+	float m = cut.y/cut.x;
+	float c = cut.y - m*cut.x;
+
+	// Calculates the intersection point
+	float x = (c - c_l) / (m_l - m);
+	float y = m_l * x + c_l;
+
+	return vec2(x,y);
 }
 
 }
